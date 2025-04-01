2e5c94e3f66c0ec4f53668591b72f6a6e5cf6f5d
BreakingBeakers <benjamin.brickner03@gmail.com>
2025-04-01 00:00:36 -0400

    Update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/drafting-magic/',
   plugins: [react()],
   server: {
     host: '192.168.1.109',
